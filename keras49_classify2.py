8150e112f550cef593242431e1ae2431456d52c1
keumdohoon <58944180+keumdohoon@users.noreply.github.com>
2020-05-26 20:57:31 +0900

    202005262057

--- keras49_classify2.py
+++ keras49_classify2.py
@@ -14,26 +14,71 @@ es = EarlyStopping(monitor = 'loss', mode = 'min', patience = 10)
 
 # 2. 데이터
 x = np.array(range(1, 11))
-y = np.array([1, 1, 2, 3, 4, 5,  2, 3, 4, 5])#.reshape(-1, 1)
+y = np.array([1, 2, 3, 4, 5, 1, 2, 3, 4, 5])#.reshape(-1, 1)
 
 # enc.fit(y)
 # y = enc.transform(y).toarray()
-y = to_categorical(y)
-y = y[:, 1:]
-print(x)
-print(x.shape)
-print(y)
-print(y.shape)
+y = to_categorical(y) #y를 1과 0으로 표현해주는것 
+# print(y)
+# [[0. 1. 0. 0. 0. 0.] 얘는 1
+#  [0. 0. 1. 0. 0. 0.] 애는 2
+#  [0. 0. 0. 1. 0. 0.] 애는 3
+#  [0. 0. 0. 0. 1. 0.] 애는 4
+#  [0. 0. 0. 0. 0. 1.] 애는 5
+#  [0. 1. 0. 0. 0. 0.] 얘는 1
+#  [0. 0. 1. 0. 0. 0.]
+#  [0. 0. 0. 1. 0. 0.]
+#  [0. 0. 0. 0. 1. 0.]
+#  [0. 0. 0. 0. 0. 1.]]
 
+y = y[:, 1:]  #이게 앞에 앞자리에 0들을 지워주는거
+# print(y)
+# [[1. 0. 0. 0. 0.]
+#  [0. 1. 0. 0. 0.]
+#  [0. 0. 1. 0. 0.]
+#  [0. 0. 0. 1. 0.]
+#  [0. 0. 0. 0. 1.]
+#  [1. 0. 0. 0. 0.]
+#  [0. 1. 0. 0. 0.]
+#  [0. 0. 1. 0. 0.]
+#  [0. 0. 0. 1. 0.]
+#  [0. 0. 0. 0. 1.]]
+
+print(x) 
+#[ 1  2  3  4  5  6  7  8  9 10]
+print(x.shape)
+#(10,)
+print(y)
+# [[1. 0. 0. 0. 0.]
+#  [0. 1. 0. 0. 0.]
+#  [0. 0. 1. 0. 0.]
+#  [0. 0. 0. 1. 0.]
+#  [0. 0. 0. 0. 1.]
+#  [1. 0. 0. 0. 0.]
+#  [0. 1. 0. 0. 0.]
+#  [0. 0. 1. 0. 0.]
+#  [0. 0. 0. 1. 0.]
+#  [0. 0. 0. 0. 1.]]
+print(y.shape)
+#(10, 5)
 
 # 3. 모델 구성
 input1 = Input(shape = (1, ))
-dense1 = Dense(100, activation = 'relu')(input1)
-dense2 = Dense(200, activation = 'relu')(dense1)
-dense3 = Dense(50, activation = 'relu')(dense2)
-dense4 = Dense(40, activation = 'relu')(dense3)
+dense1 = Dense(3, activation = 'relu')(input1)
+dense2 = Dense(6, activation = 'relu')(dense1)
+dense3 = Dense(12, activation = 'relu')(dense2)
+dense4 = Dense(24, activation = 'relu')(dense3)
+dense5 = Dense(48, activation = 'relu')(dense4)
+dense6 = Dense(96, activation = 'relu')(dense5)
+dense7 = Dense(192, activation = 'relu')(dense6)
+dense8 = Dense(96, activation = 'relu')(dense7)
+dense8 = Dense(48, activation = 'relu')(dense7)
+dense8 = Dense(24, activation = 'relu')(dense7)
+dense8 = Dense(12, activation = 'relu')(dense7)
 
-output1 = Dense(10)(dense4)
+dense9 = Dense(6, activation = 'relu')(dense8)
+
+output1 = Dense(3)(dense9)
 output2 = Dense(5, activation = 'softmax')(output1)
 
 model = Model(inputs = input1, outputs = output2)
@@ -44,7 +89,7 @@ model.summary()
 # 4. 컴파일 및 훈련
 model.compile(loss = 'categorical_crossentropy',
               optimizer = 'adam', metrics = ['accuracy'])
-model.fit(x, y, epochs = 100, batch_size = 1)
+model.fit(x, y, epochs = 122, batch_size = 1)
 
 
 # 5. 평가 및 예측
@@ -52,9 +97,9 @@ loss, acc = model.evaluate(x, y, batch_size = 1)
 print("loss : ", loss)
 print("acc : ", acc)
 
-x_pred = np.array([1, 2, 3])
+x_pred = np.array([1, 2, 3, 4, 5])
 y_pred = model.predict(x_pred)
-# y_pred = np.argmax(y_pred, axis = 1).reshape(-1, )
+y_pred = np.argmax(y_pred, axis = 1)+1
 # y_pred = enc.transform(y_pred)
-y_pred = to_categorical(y_pred)
+#y_pred = to_categorical(y_pred)
 print("y_pred : \n", y_pred)
--- keras50_cnn1.py
+++ keras50_cnn1.py
@@ -7,13 +7,16 @@ model.add(Conv2D(10, (2,2), input_shape= (10,10,1)))   #(9,9,10)
 # CNN은 4차원이다.CNN은 한번 자른것 가지고 데이터의 특성을 알아내지 못하니 한번 자른걸 자르고 또 자른다. 
 model.add(Conv2D(7, (3,3))) ##(7,7,7)
 model.add(Conv2D(5, (2,2), padding = 'same'))       #(7,7,5)
-model.add(Conv2D(5, (2, 2)))              #(3, 3, 5)
+model.add(Conv2D(5, (2,2)))
+#model.add(Conv2D(5, (2, 2), strides= 2))              #(3, 3, 5)
+#model.add(Conv2D(5, (2,2)), stride=2, padding='same')#이렇게 되면 패딩값보다 스트라이드 값을 먼저 쳐준다. 
 model.add(MaxPooling2D(pool_size=2))
-model.add(Flatten)
+model.add(Flatten())
 model.add(Dense(1))
 model.summary()
 #지금 댄스의 레이어가 4차
 # 여기서 padding의 디폴트 값은 valid 이다. 
-# stride띄어주는 것 은 항상 디폴트값 1로 되어있다. 
+# stride(띄어주는, 몇개씩점프를 하는지) 것 은 항상 디폴트값 1로 되어있다. 
 # model.add(Conv2D(10, kernel_size=2, input_shape = (5,5,1)))
+#maxpooling이란 전체 그림에서 가장 중요한 곳들만을 빼와서 하나를 만들어 주는 것이다 만들때 풀사이즈는 2이다. 
 #flatten은 데이터를 4차원의 데이터를 쭈욱 펴준다는 의미이다. 그위에 있는 (n,3,3,5)를 다 곱해준 값이다 즉 45가 된다. convolutionlayer에 끝은 항상 flatten이다. 
