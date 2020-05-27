71d815153ea4337e182a3c17f0da067b33c6a4a1
keumdohoon <58944180+keumdohoon@users.noreply.github.com>
2020-05-27 13:53:21 +0900

    202005271353

--- keras48.5.과제.py
+++ keras48.5.과제.py
@@ -39,9 +39,9 @@ model.add(Dense(20))
 model.add(Dense(40))   
 model.add(Dense(110))   
 model.add(Dense(100)) 
-model.add(Dense(80))   
+model.add(Dense(800))   
 model.add(Dense(60)) 
-model.add(Dense(20))   
+model.add(Dense(200))   
 model.add(Dense(10)) 
 model.add(Dense(1,  activation='sigmoid'))
 #마지막에 나오는 활성화 값이 시그모이드를 붙이면 0아니면 1이 나오게 된다. 마지막레이어의 값 곱하기 시그모이드. 
@@ -57,7 +57,7 @@ es = EarlyStopping(monitor = 'loss', patience=100, mode = 'auto')
 #model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 #	return model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics = ['accuracy'] )
-model.fit(x, y, epochs=100, batch_size=1, verbose=2)
+model.fit(x, y, epochs=1220, batch_size=32, verbose=2)
 #0이냐 1이냐 딱딱 떨어지는 숫자를 찾는 것이기에 우리는 여기서 mse가 아닌 딱딱 떨어지는 숫자를 나타내주는  acc를 사용하게 된다. 
 #binary_crossentropy 2진분류에서 binarycrossentropy딱 하나밖에 없다. 이걸 외울것. 
 #1, 아웃풋에 시그모이드 추가, 2,model.compile에 loss를 binary crossentropy로 바꾸어주고 , metrics를 accuracy로 바꾸어주게된다. 
@@ -79,7 +79,7 @@ x_pred = np.array([1,2,3])
 y_pred = model.predict(x_pred)
 print('y_pred :', y_pred)
 # sigmoid 함수를 안거친것으로 보여지게 된다. 
-y1_pred = np.where(y_pred >= 0.5, 1, 0)     
+y1_pred = np.round(y_pred)     
 #sigmoid를 써줘서 모든 값이 0과 1사이인 값으로 나오게 되는데 np.where을 사용해주면, 0.5를 기준으로 더 크거나 같은 값은 1 그리고 이 조건에 충족하지 못하는 값은 0이 된다. 
 print('y_pred :', y1_pred)
 
