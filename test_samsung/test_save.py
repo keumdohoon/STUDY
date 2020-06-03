7a39bfb4b843993b0e4b70cf0af1921b8c1e9d7d
keumdohoon <58944180+keumdohoon@users.noreply.github.com>
2020-06-03 15:44:35 +0900

    202006031544

---
+++ test_samsung/test_save.py
@@ -0,0 +1,132 @@
+#하이트 주가를 불러온다 
+import numpy as np
+import pandas as pd
+
+
+datasets1 = pd.read_csv("./data/csv/하이트 주가.csv", 
+                        index_col = 0, 
+                        header = 0,encoding = 'cp949', sep =',')
+
+# print(datasets1)       
+print(datasets1.shape)      
+
+#삼성주가 파일을 불러온다.
+datasets2 = pd.read_csv("./data/csv/삼성전자 주가.csv", 
+                        index_col = 0, 
+                        header = 0,encoding = 'cp949', sep =',')
+
+# print(datasets2)       
+print(datasets2.shape)        
+
+
+datasets1 = datasets1.dropna()
+datasets2 = datasets2.dropna()
+
+# datasets2 = pd.DataFrame(datasets2, index = [])
+
+# datasets2.drop(['2020-06-02', '2020-06-01'])
+datasets2 = datasets2.drop(datasets2.index[0])
+
+
+print(datasets1)
+print(datasets2)
+print(datasets1.shape)  #(508, 5)
+print(datasets2.shape)  #(508, 1)
+
+
+#거래량str->int 변경
+#hite
+for i in range(len(datasets1.index)): 
+    datasets1.iloc[i,1] = int(datasets1.iloc[i,1].replace(',',''))
+
+#samsung
+for i in range(len(datasets2.index)): 
+    for j in range(len(datasets2.iloc[i])):
+        datasets2.iloc[i,j] = int(datasets2.iloc[i,j].replace(',',''))
+
+# datasets1=np.transpose(datasets1)
+
+print(datasets1)
+print(datasets2)
+
+datasets1 = datasets1.sort_values(['일자'], ascending=[True])
+datasets2 = datasets2.sort_values(['일자'], ascending=[True])
+print('dataset1:', datasets1)
+print('dataset2:', datasets2)
+
+
+#일자순으로 깔끔하게 나오게 분류해준다.
+
+print(datasets1.values)
+datasets2 = datasets2.values
+#pandas를 numpy로 바꿔준다
+
+print('======================================')
+print('x',datasets1)
+print('x',datasets2)
+
+
+
+print(datasets1.shape)  #(426, 5)(508, 5)
+print(datasets2.shape)  #(426, 5)(508, 1)
+print(type(datasets1), type(datasets2))
+#<class 'numpy.ndarray'> <class 'numpy.ndarray'>로 우리가 원하는 방향으로 변경된걸 알 수 있다.
+
+
+
+# datasets1=np.transpose(datasets1)
+# print(datasets1)
+# print(datasets1.shape)
+# print(len(datasets1), '시가')
+
+# datasets1 = pd.datasets1({'시가': ['1', '2', '3'], 
+
+#                    '고가': ['4.1', '5.5', '6.0'], '저가': ['4.1', '5.5', '6.0'], '종가': ['4.1', '5.5', '6.0'], '거래량': ['4.1', '5.5', '6.0']}) 
+
+
+
+# print(datasets1)
+
+
+# from pandas import DataFrame
+# import numpy as np
+ 
+# data = [
+    
+#     ]
+ 
+# columns = ["시가", "고가", "저가", "종가", "거래량"]
+# datasets1 = DataFrame(data=data, columns=columns)
+# print(datasets1)
+
+
+def remove_comma(x):
+         return x.replace(',', '')
+datasets1['시가'] = datasets1['시가'].apply(remove_comma)
+datasets1['저가'] = datasets1['저가'].apply(remove_comma)
+datasets1['종가'] = datasets1['종가'].apply(remove_comma)
+datasets1['거래량'] = datasets1['거래량'].apply(remove_comma)
+print(datasets1)
+print(datasets1.dtypes)
+
+datasets1 = datasets1.astype({'시가': np.int64})
+print(datasets1)
+print(datasets1.dtypes)
+datasets1 = datasets1.astype({'고가':np.int64})
+datasets1 = datasets1.astype({'저가': np.int64})
+datasets1 = datasets1.astype({'종가': np.int64})
+datasets1 = datasets1.astype({'거래량': np.int64})
+print(datasets1)
+print(datasets1.dtypes)
+
+
+print('x',datasets1)
+print('x',datasets2)
+
+#데이터를 넘파이로 저장하기
+np.save('./data/hite.npy', arr = datasets1) 
+np.save('./data/samsung_electronics.npy', arr = datasets2) 
+print(datasets1)
+print(datasets2)
+
+#실행시키면 저장까지 다 한것이다.
