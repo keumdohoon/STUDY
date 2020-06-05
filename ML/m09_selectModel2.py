bd336bdda9aa5aeae3b318d6c022d63843c9772a
keumdohoon <58944180+keumdohoon@users.noreply.github.com>
2020-06-05 18:41:54 +0900

    202006051600

---
+++ ML/m09_selectModel2.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.utils.testing import all_estimators
+import warnings
+from sklearn.datasets import load_boston
+from sklearn.metrics import r2_score
+
+warnings.filterwarnings('ignore')
+
+
+
+boston = load_boston()
+print(boston)
+x = boston.data
+y = boston.target
+
+# boston = pd.read_csv('./data/csv/boston_house_prices.csv', header = 0)
+
+
+# print(boston)
+# print(boston.shape)
+# print(type(boston))
+
+
+
+# x = boston.iloc[:, 0:13]
+# y = boston.iloc[:, 13]
+
+print(x)
+print(y)
+# warnings.filterwarning('ignore')
+
+
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size =0.2, random_state = 66)
+# warnings.filterwarning('ignore')
+
+
+allAlgorithms = all_estimators(type_filter = 'regressor')
+
+for (name, algorithm) in allAlgorithms:
+        model = algorithm()
+    
+        model.fit(x_train, y_train)
+        y_pred = model.predict(x_test)
+        print(name, "의 정답률 =", r2_score(y_test, y_pred))
+
+
+import sklearn
+print(sklearn.__version__)
+# warnings.filterwarning('ignore')
