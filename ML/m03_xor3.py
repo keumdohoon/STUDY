770698b22a525a50f046289ef84a8e76516d4ecb
keumdohoon <58944180+keumdohoon@users.noreply.github.com>
2020-06-07 23:02:18 +0900

    20200607

--- ML/m03_xor3.py
+++ ML/m03_xor3.py
@@ -14,7 +14,7 @@ y_data = [0,1,1,0]
 model = LinearSVC()
 model = SVC()
 model = KNeighborsClassifier(n_neighbors=1)
-#neighbors 가 각 객체를 1씩 연결하겠다. 두개씩 하면 정확도가 떨어진다. 
+#Kneighbors 가 각 객체를 1씩 연결하겠다. 두개씩 하면 정확도가 떨어진다. 한개의 객체에 한개씩의 보이지 않는 선이 연결되는 방식이다. 
 
 #3. 실행
 model.fit(x_data, y_data)
@@ -24,7 +24,8 @@ x_test = [0,0], [1, 0], [0,1], [1,1]
 y_predict = model.predict(x_test)
 
 
-acc = accuracy_score([0,1,1,0], y_predict)
+acc = accuracy_score(y_data, y_predict)
+#accuracy_score 뒤에는 (y_true, y_pred, *, normalize=true{디폴트값}, sample_weight = none 이 들어가게 되는것이다.)
 #accuracy score, and score is the substitute of evaluate from keras evaluate= score 
 print(x_test, "의 예측 결과 :", y_predict)
 print("acc = ", acc)
