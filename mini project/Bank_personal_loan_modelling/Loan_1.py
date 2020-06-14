c9e63c5a1194d7d937d74da17c23a5c59046d99c
keumdohoon <58944180+keumdohoon@users.noreply.github.com>
2020-06-15 00:24:48 +0900

    first trial error when opening file

---
+++ mini project/Bank_personal_loan_modelling/Loan_1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+# import matplotlib.inline
+
+
+
+import os
+# print(os.listdir("./input"))
+
+original = pd.read_excel('./Bank_personal_loan_modelling.xlsx',"Data")
---
+++ mini project/Bank_personal_loan_modelling/Loan_data information.py
@@ -0,0 +1,16 @@
+#5000개의 행과 14개의 열로 이루어진 데이터
+#ID Customer ID
+# Age Customer's age in completed years
+# Experience #years of professional experience
+# Income Annual income of the customer (USD 1000)
+# ZIPCode Home Address ZIP code.
+# Family Family size of the customer
+# CCAvg Avg. spending on credit cards per month (USD 1000)
+# Education Education Level. 1: Undergrad; 2: Graduate; 3: Advanced/Professional
+# Mortgage Value of house mortgage if any. (USD 1000)
+# Personal Loan Did this customer accept the personal loan offered in the last campaign?
+# Securities Account Does the customer have a securities account with the bank?
+# CD Account Does the customer have a certificate of deposit (CD) account with the bank?
+# Online Does the customer use internet banking facilities?
+# CreditCard Does the customer uses a credit card issued by UniversalBank?
+#위에가 14개의 컬럼이다.
