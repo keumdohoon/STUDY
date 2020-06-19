2d23d0a9a6ccd626f10351e9b02897abe0b3785b
keumdohoon <58944180+keumdohoon@users.noreply.github.com>
2020-06-19 16:31:52 +0900

    XGB basic info

--- ML/m21_XGB.py
+++ ML/m21_XGB.py
@@ -3,7 +3,8 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from xgboost import XGBClassifier
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 cancer = load_breast_cancer()
@@ -15,10 +16,14 @@ x_train, x_test, y_train, y_test = train_test_split(
 # model = RandomForestClassifier()
 # model = GradientBoostingClassifier()
 model = XGBClassifier()
+# max_features : 기본값 쓸것!
+# n_estimators : 클수록 좋다!, 단점 메모리를 많이 차지 한다는것을 인지하고 기본값은 100이다. 
+# n_jobs = -1  : 병렬 처리( *주의 gpu 같이 돌릴땐 2이상 값을 주면 터진다)
+
 
 #max_features : 기본값 써라!
 # n_estimators : the bigger the better클수록 좋은거다, 단점으로는 메모리를 짱 차지한다 , 기본값 100
-#n_jobs = -1 : 병렬처리 
+#n_jobs = -1 : 병렬처리 (gpu 같이 돌릴때는 2이상의 값을 쓸때는 터진다.우리는 12코어를 사용하고 12개를 다 사용해주기 위해서 -1즉 전체를 다써준다는 얘기다. )
 
 model.fit(x_train, y_train)
 
@@ -27,8 +32,7 @@ acc = model.score(x_test , y_test)
 print(model.feature_importances_)
 print(acc)
 
-import matplotlib.pyplot as plt
-import numpy as np
+
 
 def plot_feature_importances_cancer(model):
     n_features = cancer.data.shape[1]
@@ -38,9 +42,14 @@ def plot_feature_importances_cancer(model):
     plt.xlabel("Feature Importances")
     plt.ylabel("Features")
     plt.ylim(-1, n_features)
+
 #이거를 분석을 하고 이것을 데이콘에 써먹을수 있으니 적용하도록 하자
 #직접 찾아보기 
 #barh, stick , lim 이 뭔지 찾아서 주석처리하기 
 plot_feature_importances_cancer(model)
 plt.show()
 #과제: 데이콘에 이 featureimportance를 적용하여 확인을 할것 
+#feature importances는 feature enginerring 이라고 한다.
+
+
+
