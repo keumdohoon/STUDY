d44640afa3d0fc1b71035fd95b034915b2c53b23
keumdohoon <58944180+keumdohoon@users.noreply.github.com>
2020-06-22 12:21:25 +0900

    number 파일 정리

---
+++ ML/m29_eval0.py
@@ -0,0 +1,22 @@
+'''
+m281.2.3 만들것
+
+1. eval에 'loss'와 다른지표 1 개 더 추가.
+2. earlystopping  적용
+3. plot 으로 그릴것. 
+
+
+
+Select from model  에 적용시켜라 
+1. 회기        m29_eval1
+2. 이진분류     m29_eval2
+3. 다중분류     m29_eval3
+
+1. eval에 'loss'와 다른지표 1 개 더 추가.
+2. earlystopping  적용
+3. plot 으로 그릴것. 
+
+4. 결과는 주석으로 소스 하단에 표시 
+
+5. m27~29까지 완벽히 이해할것 
+'''
