563ca77e82d2bfbc584501e7e2c24a405e679cbc
keumdohoon <58944180+keumdohoon@users.noreply.github.com>
2020-06-24 17:00:32 +0900

    정리

--- ML/m01.py
+++ ML/m01.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 x = np.arange(0,10,0.1)
 #0~10까지를 0.1번씩 움직여가면서 그려라
-y = np.sin(x)
+y = np.sin(x) #x에 대한 sin값을 알려주는 것이다. 
 
 plt.plot(x,y)
 #x값을 넣으면 0.1씩 증가하고 y는 그것에 대한 각각의 싸인 값을 준다 0.1의 싸인값0.2의 싸인값, 0.3의 싸인값
