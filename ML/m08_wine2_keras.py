e6328e1e8dd931ddb5661c03a1785bd30f081e5c
keumdohoon <58944180+keumdohoon@users.noreply.github.com>
2020-06-25 23:49:58 +0900

    .

--- ML/m08_wine2_keras.py
+++ ML/m08_wine2_keras.py
@@ -6,38 +6,41 @@ from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
+import pandas as pd
+from keras.utils.np_utils import to_categorical
+from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 
 # 1. data
-import numpy as np
-wine = np.genfromtxt('./data/csv/winequality-white.csv',delimiter=';')
-print(wine)
+# wine = np.genfromtxt('./data/csv/winequality-white.csv',delimiter=';')
 
-x_wine = wine[:,0:11]
-y_wine = wine[:,11]
+dataset = pd.read_csv('./data/csv/winequality-white.csv', index_col = None, header = 0, sep =  ';')
+
+np_dataset = dataset.values
+
+print(np_dataset.shape) #(4898, 12)
+
+x = np_dataset[:,0:-1]
+y = np_dataset[:,-1]
 #d이 슬라이싱 방식은 numpy형식일때 사용 된다, 우리가 이미 numpy로 바꾸어서 정보를 가져와서 이렇게 사용할수 있다.
 
-# print(y_wine)
+# one hot
+y = to_categorical(y)
 
-print(x_wine.shape)#(4899, 11)
-print(y_wine.shape)#(4899,)
+print(x.shape)#(4899, 11)
+print(y.shape)#(4898, 10)
 
+#scaler
+scaler = MinMaxScaler()
+scaler.fit(x)
+x= scaler.transform(x)
 
+x_train,x_test, y_train,y_test = train_test_split(x,y,
+                                                  random_state = 33, shuffle=True,
+                                                  train_size=0.8)
 
-# y_wine = np_utils.to_categorical(y_wine)
-
-
-x_train,x_test, y_train,y_test = train_test_split(x_wine,y_wine,
-                                                  random_state = 66, shuffle=True,
-                                                  train_size=0.9)
-
-scaler = StandardScaler()
-scaler.fit(x_train)
-x_train = scaler.transform(x_train)
-x_test = scaler.transform(x_test)
-
-print(x_train.shape) # 4408,11
-print(y_train.shape) # 4408,10
+print(x_train.shape) # (3918, 11)
+print(y_train.shape) # (3918, 10)
 
 # 2. model
 model = Sequential()
@@ -46,13 +49,15 @@ model.add(Dense(30))
 model.add(Dense(30))
 model.add(Dense(10, activation='softmax'))
 
+# EarlyStopping
+es = EarlyStopping(monitor = 'val_loss', mode = 'auto', patience= 3)
+
 # 3. compile, fit
 model.compile(optimizer='adam',loss = 'categorical_crossentropy', metrics = ['acc'])
-
-model.fit(x_train,y_train,epochs=30,batch_size=64,callbacks=[],verbose=2,validation_split=0.1)
+model.fit(x_train,y_train,epochs=30,batch_size=64,callbacks=[es],verbose=2,validation_split=0.2)
 
 # 4. 평가, 예측
-loss,acc = model.evaluate(x_test,y_test,batch_size=3)
+loss,acc = model.evaluate(x_test,y_test,batch_size=64)
 
 print("loss : ",loss)
 print("acc : ",acc)
