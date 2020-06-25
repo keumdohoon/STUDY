75e25823921c37e0f24aabbdfecbe273cfc04f9b
keumdohoon <58944180+keumdohoon@users.noreply.github.com>
2020-06-25 23:48:42 +0900

    .

--- ML/m03_xor3_practice.py
+++ ML/m03_xor3_practice.py
@@ -15,7 +15,7 @@ x_data = [[0,0], [1,0], [0,1], [1,1]]
 y_data = [0,1,1,0]
 
 #2. model 
-model = LinearSVC()
+# model = LinearSVC()
 model = SVC()
 model = KNeighborsClassifier(n_neighbors=1)
 
@@ -29,3 +29,5 @@ y_predict = model.predict(x_test)
 acc = accuracy_score([0,1,1,0], y_predict)
 print(x_test, "의 예측 결과 :", y_predict)
 print("acc = ", acc)
+#([0, 0], [1, 0], [0, 1], [1, 1]) 의 예측 결과 : [0 1 1 0]
+# acc =  1.0 
