164dc0f9b55429005f328a387a1d7ce11a32279a
keumdohoon <58944180+keumdohoon@users.noreply.github.com>
2020-06-30 17:31:04 +0900

    setting up directories

---
+++ AIChallenge2020/Keum/model/directory.py
@@ -0,0 +1,17 @@
+from keras.preprocessing import image_dataset_from_directory
+
+image_dataset_from_directory(
+    '/tf/notebooks/Keum/data/train/',
+    labels="inferred",
+    label_mode="int",
+    class_names=None,
+    color_mode="rgb",
+    batch_size=32,
+    image_size=(256, 256),
+    shuffle=True,
+    seed=None,
+    validation_split=None,
+    subset=None,
+    interpolation="bilinear",
+    follow_links=False,
+)
