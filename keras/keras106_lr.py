cbb23453b9bc6e8c5aae483b27a47bc57760eaf2
keumdohoon <58944180+keumdohoon@users.noreply.github.com>
2020-07-02 20:47:32 +0900

    learning rate explained down&up error

---
+++ keras/keras106_lr.py
@@ -0,0 +1,26 @@
+#lr  가 어떤 방식으로 적용 되는지. 
+
+weight = 0.5
+input = 0.5
+goal_prediction = 0.8
+lr = 0.001 
+lr = 0.1
+lr = 1
+lr = 0.0001
+
+for iteration in range(1101):#1100번 돌려라 
+
+    prediction = input * weight
+    error = (prediction - goal_prediction) **2
+
+    print("Error : " +str(error) + "/tPrediction : " + str(prediction))
+
+    up_prediction = input* (weight +lr)
+    up_error = (goal_prediction - up_prediction) **2
+
+    down_prediction = input* (weight - lr)
+    down_error = (goal_prediction - down_prediction) **2
+    if(down_error < up_error) :
+        weight = weight  - lr
+    if(down_error > up_error) :
+        weight = weight  + lr 
