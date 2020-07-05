2e3e92727723e9b48471b1521d2c3ba40cb04ca3
keumdohoon <58944180+keumdohoon@users.noreply.github.com>
2020-07-05 17:55:42 +0900

    need_review

---
+++ Book_review/Hands_on_machine_learning(homl)/homl_ch10_389_subclassing_api.py
@@ -0,0 +1,20 @@
+
+#389쪽 서브틀래싱 API로 동적 모델 만들기
+class WideAndDeepModel(keras.models.Model):
+    def __init__(self, units=30, activation="relu", **kwargs):
+        super().__init__(**kwargs)
+        self.hidden1 = keras.layers.Dense(units, activation=activation)
+        self.hidden2 = keras.layers.Dense(units, activation=activation)
+        self.main_output = keras.layers.Dense(1)
+        self.aux_output = keras.layers.Dense(1)
+        
+    def call(self, inputs):
+        input_A, input_B = inputs
+        hidden1 = self.hidden1(input_B)
+        hidden2 = self.hidden2(hidden1)
+        concat = keras.layers.concatenate([input_A, hidden2])
+        main_output = self.main_output(concat)
+        aux_output = self.aux_output(hidden2)
+        return main_output, aux_output
+
+model = WideAndDeepModel(30, activation="relu")
