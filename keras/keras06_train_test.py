2b695b72014acfd26e215bc11c4b301efa9a4d49
keumdohoon <58944180+keumdohoon@users.noreply.github.com>
2020-07-23 23:55:39 +0900

    keum

--- keras/keras06_train_test.py
+++ keras/keras06_train_test.py
@@ -5,7 +5,8 @@ y_train = np.array([1,2,3,4,5,6,7,8,9,10])
 x_test = np.array([11,12,13,14,15])
 y_test = np.array([11,12,13,14,15])
 x_pred = np.array([16, 17, 18])
-
+#train 으로 훈련을 시키고 test으로 어느정도 맞는지를 확인한 다음에 acc와 loss가 알맞게 나온다면 
+#그 가중치를 가지고 x_pred를 모델 프리딕트해준다면 y_pred를 찾아줄수 있다. 
 #2. 모델구성
 from keras.models import Sequential
 from keras.layers import Dense
@@ -30,7 +31,7 @@ model.add(Dense(1))
 #3. 훈련
 model.compile(loss='mse', optimizer='adam', metrics=['mse'])
 model.fit(x_train, y_train, epochs=110, batch_size=1)
-
+#x_train, y_train 을 
 #4. 평가와 예측
 loss, mse = model.evaluate(x_test, y_test, batch_size=1)
 print("loss ; ", loss)
