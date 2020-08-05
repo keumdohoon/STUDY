5752b36f0d22384be04c255dafa0d996716a77ed
keumdohoon <58944180+keumdohoon@users.noreply.github.com>
2020-08-05 11:07:16 +0900

    alot of graph

---
+++ AE/A07_ae2_manygraph.py
@@ -0,0 +1,95 @@
+
+import matplotlib.pyplot as plt
+import random
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+def autoencoder(hidden_layer_size):
+    model = Sequential()
+    model.add(Dense(units=hidden_layer_size, input_shape=(784,), activation='relu'))
+    model.add(Dense(units=784, activation='sigmoid'))
+    return model
+
+from tensorflow.keras.datasets import mnist
+
+train_set, test_set = mnist.load_data()
+x_train, y_train = train_set
+x_test, y_test = test_set
+
+x_train = x_train.reshape(x_train.shape[0], x_train.shape[1] * x_train.shape[2]).astype('float32')/255.
+x_test = x_test.reshape(x_test.shape[0], x_test.shape[1] * x_test.shape[2]).astype('float32')/255.
+print(x_train.shape, x_test.shape)
+
+model_01 = autoencoder(hidden_layer_size=1)
+model_02 = autoencoder(hidden_layer_size=2)
+model_04 = autoencoder(hidden_layer_size=4)
+model_08 = autoencoder(hidden_layer_size=8)
+model_16 = autoencoder(hidden_layer_size=16)
+model_32 = autoencoder(hidden_layer_size=32)
+
+model_01.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+# model.compile(optimizer='adam', loss='mse', metrics=['acc'])
+model_01.fit(x_train, x_train, epochs=10)
+
+model_02.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+model_02.fit(x_train, x_train, epochs=10)
+
+model_04.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+model_04.fit(x_train, x_train, epochs=10)
+
+model_08.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+model_08.fit(x_train, x_train, epochs=10)
+
+model_16.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+model_16.fit(x_train, x_train, epochs=10)
+
+model_32.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+model_32.fit(x_train, x_train, epochs=10)
+
+output_01 = model_01.predict(x_test)
+output_02 = model_02.predict(x_test)
+output_04 = model_04.predict(x_test)
+output_08 = model_08.predict(x_test)
+output_16 = model_16.predict(x_test)
+output_32 = model_32.predict(x_test)
+
+
+# 그림 그리자
+fig, axes = plt.subplots(7, 5, figsize=(15, 15))
+
+random_imgs = random.sample(range(output_01.shape[0]), 5)
+outputs = [x_test, output_01, output_02, output_04,
+                   output_08, output_16, output_32]
+
+for row_num, row in enumerate(axes):
+    for col_num, ax in enumerate(row):
+        ax.imshow(outputs[row_num][random_imgs[col_num]].reshape(28, 28), cmap='gray')
+        ax.grid(False)
+        ax.set_xticks([])
+        ax.set_yticks([])
+plt.show()\
+
+# fig, ((ax1, ax2, ax3, ax4, ax5), (ax6, ax7, ax8, ax9, ax10)) = plt.subplots(2, 5, figsize = (20, 7))
+
+# # 이미지 다섯 개를 무작위로 고른다.
+# random_images = random.sample(range(output.shape[0]), 5)
+
+# # 원본(입력) 이미지를 맨 위에 그린다.
+# for i, ax in enumerate([ax1, ax2, ax3, ax4, ax5]):
+#     ax.imshow(x_test[random_images[i]].reshape(28, 28), cmap = 'gray')
+#     if i ==0:
+#         ax.set_ylabel('INPUT', size = 40)
+#     ax.grid(False)
+#     ax.set_xticks([])
+#     ax.set_yticks([])
+
+# # 오토인코더가 출력한 이미지를 아래에 그린다.
+# for i, ax in enumerate([ax6, ax7, ax8, ax9, ax10]):
+#     ax.imshow(output[random_images[i]].reshape(28, 28), cmap = 'gray')
+#     if i ==0:
+#         ax.set_ylabel('OUTPUT', size = 40)
+#     ax.grid(False)
+#     ax.set_xticks([])
+#     ax.set_yticks([])
+# plt.show()                                                                         
