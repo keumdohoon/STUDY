fba34a247d02f9ae6e4f4b165eb7d8c8111cc91e
keumdohoon <58944180+keumdohoon@users.noreply.github.com>
2020-08-08 13:32:04 +0900

    autoencoder

---
+++ AE/A09_ae4_cnn.py
@@ -0,0 +1,49 @@
+#20200804
+#a08 복붙
+#CNN으로 오토인코더를 구성하시오.
+
+import tensorflow as tf
+
+def autoencoder():
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Conv2D(filters = 32, kernel_size=(3,3),
+                                    padding = 'same', input_shape=(28,28,1),
+                                    activation = 'relu')),
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2))),
+    model.add(tf.keras.layers.Conv2D(filters = 16, kernel_size = (3,3),
+                                        padding = 'same', activation = 'relu')),
+    model.add(tf.keras.layers.UpSampling2D(size=(2,2))),
+    model.add(tf.keras.layers.Conv2D(filters=8, kernel_size=(3,3),
+                                     padding='same', activation='relu')),
+    model.add(tf.keras.layers.Conv2D(filters=16, kernel_size=(3,3),
+                                     padding='same', activation='relu')),
+    model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=(3,3),
+                                     padding='same', activation='relu')),
+    model.add(tf.keras.layers.Conv2D(filters=1, kernel_size=(3,3),
+                                     padding='same', activation='sigmoid'))
+    return model
+
+
+#데이터
+train_set, test_set = tf.keras.datasets.mnist.load_data()
+x_train, y_train = train_set
+x_test, y_test = test_set
+
+x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], x_train.shape[2], 1))
+x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], x_test.shape[2],1))
+x_train = x_train /255.
+x_test = x_test /255.
+
+print(x_train.shape)
+print(x_test.shape)
+
+model = autoencoder()
+model.summary()
+
+
+# model.compile(optimizer='adam', loss='mse', metrics=['acc'])  # loss = 0.01
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])    # loss = 0.09
+
+model.fit(x_train, x_train, epochs = 10)
+
+
---
+++ AE/A11_noise1.py
@@ -0,0 +1,77 @@
+# 202008050915
+
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import numpy as np
+def autoencoder(hidden_layer_size):
+    model = Sequential()
+    model.add(Dense(units = hidden_layer_size, input_shape=(784, ), activation = 'relu'))
+    model.add(Dense(units = 784, activation = 'sigmoid'))
+
+    return model
+
+from tensorflow.keras.datasets import mnist
+
+train_set, test_set = mnist.load_data()
+x_train, y_train = train_set
+x_test, y_test = test_set
+
+x_train = x_train.reshape((x_train.shape[0], x_train.shape[1]*x_train.shape[2]))
+x_test = x_test.reshape((x_test.shape[0], x_test.shape[1]*x_test.shape[2]))
+x_train = x_train/255.
+x_test = x_test/255.
+
+
+######################
+#add some noise#####
+# 입력 데이터에 노이즈 생성 ; 픽셀에 랜덤하게 0을 뿌려준다
+# 문제가 있다 ; / 255.를 해줌으로써 현재 데이터의 분포는 0 ~ 1 사이에 있음
+# 평균이 0, 표준편차가 0.5면 0 ~ 1 사이의 범위를 벗어날 수 있음
+
+x_train_noised = x_train + np.random.normal(0,0.1,size = x_train.shape)
+#0과 0.5에 따른 정규 분포를 알려줘라
+x_test_noised = x_test + np.random.normal(0,0.1,size = x_test.shape)
+#평균이 0 표준편차가 0.5에 음수가 들어갈 수가 있다. 그래서 우리가 원하지 않는 음수가 나오지 않게 하기 위해서 0~1사이로 지정해줄 것이다. 
+x_train_noised = np.clip(x_train_noised, a_min=0, a_max=1)
+x_test_noised = np.clip(x_test_noised, a_min=0, a_max=1)
+
+model = autoencoder(hidden_layer_size= 100)
+
+##########################
+
+
+
+# model.compile(optimizer = 'adam', loss = 'mse', metrics = ['acc'])                
+model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['acc'])  
+
+model.fit(x_train_noised,x_train_noised, epochs = 30)
+
+output = model.predict(x_test_noised)
+
+import matplotlib.pyplot as plt
+import random
+fig, ((ax1, ax2, ax3, ax4, ax5), (ax6, ax7, ax8, ax9, ax10),(ax11, ax12, ax13, ax14, ax15)) = \
+    plt.subplots(3, 5, figsize = (20, 7))
+
+# 이미지 다섯 개를 무작위로 고른다.
+random_images = random.sample(range(output.shape[0]), 5)
+
+# 원본(입력) 이미지를 맨 위에 그린다.
+for i, ax in enumerate([ax1, ax2, ax3, ax4, ax5]):
+    ax.imshow(x_test_noised[random_images[i]].reshape(28, 28), cmap = 'gray')
+    if i ==0:
+        ax.set_ylabel('INPUT', size = 40)
+    ax.grid(False)
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+# 오토인코더가 출력한 이미지를 아래에 그린다.
+for i, ax in enumerate([ax6, ax7, ax8, ax9, ax10]):
+    ax.imshow(output[random_images[i]].reshape(28, 28), cmap = 'gray')
+    if i ==0:
+        ax.set_ylabel('OUTPUT', size = 40)
+    ax.grid(False)
+    ax.set_xticks([])
+    ax.set_yticks([])
+plt.show()                                                                         
